02a8bbd0f930c3e54a51f0422b2970357cc025db
Audrey Yang <ayang.penn@gmail.com>
2024-12-08 22:22:53 -0600

    Switch auth method

--- src/main/backend/db.ts
+++ src/main/backend/db.ts
@@ -1,23 +1,15 @@
 import { app } from "electron";
-import { IamAuthenticator } from "ibm-cloud-sdk-core";
 import path from "path";
 import PouchDB from "pouchdb";
 PouchDB.plugin(require("pouchdb-find"));
 
 export const db = new PouchDB(path.join(app.getPath("sessionData"), "leveldb"));
 
-// Get Cloudant auth credentials
-const authenticator = new IamAuthenticator({
-  apikey: import.meta.env.VITE_CLOUDANT_API_KEY,
-});
-
 // Create DB and sync with remote DB
 const remoteDb = new PouchDB(`${import.meta.env.VITE_CLOUDANT_URL}`, {
-  fetch: async (url: any, opts: any) => {
-    const bearerOpts: { headers: { [key: string]: string } } = { headers: {} };
-    await authenticator.authenticate(bearerOpts);
-    opts.headers.set("Authorization", bearerOpts["headers"]["Authorization"]);
-    return PouchDB.fetch(url, opts);
+  auth: {
+    username: `${import.meta.env.VITE_CLOUDANT_USERNAME}`,
+    password: `${import.meta.env.VITE_CLOUDANT_PASSWORD}`,
   },
 });
 
