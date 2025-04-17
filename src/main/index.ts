29aa585100fc36d838e094620cbedceb3f1680e5
Audrey Yang <ayang.penn@gmail.com>
2025-04-16 20:15:34 -0500

    Fix window opening issue

--- src/main/index.ts
+++ src/main/index.ts
@@ -120,6 +120,8 @@ app.whenReady().then(() => {
     createLogoutWindow();
   });
 
+  showWindow();
+
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
